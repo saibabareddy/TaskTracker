00df49618a3057fa3b25bba5961e34b49f4dd150
Mastan Shaik <mastan30@gmail.com>
2017-08-04 23:44:51 -0400

    added Main Controller

---
+++ src/main/java/com/tasktracker/controllers/MainController.java
@@ -0,0 +1,14 @@
+package com.tasktracker.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+	
+	@RequestMapping("/")
+	public String getIndex() {
+		return "index";
+	}
+
+}
