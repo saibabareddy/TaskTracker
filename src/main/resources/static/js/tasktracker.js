58421ea2592eecd4e5253f503ac1d73e387c325f
Mastan Shaik <mastan30@gmail.com>
2017-08-14 00:15:37 -0400

    added as new

--- src/main/resources/static/js/tasktracker.js
+++ src/main/resources/static/js/tasktracker.js
@@ -369,7 +369,7 @@ function hidepopup()
 
 function progressUpdater(htmlElement,start){
 	// set progress bar
-	   var maxTime = 1800000;
+	   var maxTime = 7200000;
 	   var timeoutVal = Math.floor(maxTime/100);
 	   animateUpdate();
 
@@ -446,10 +446,10 @@ $(document).ready(function(){
 	 	var timerNow = new Date();
 	
 	
-	if(d.getUTCHours() >= 15 && d.getUTCHours() < 16 && d.getUTCMinutes() <=30 ){
-					countDate.setUTCHours(15);
-					countDate.setUTCMinutes(30);
-					countDate.setUTCSeconds(59);
+	if(d.getUTCHours() >= 15 && d.getUTCHours() < 17){
+					countDate.setUTCHours(17);
+					countDate.setUTCMinutes(00);
+					countDate.setUTCSeconds(00);
 					timerNow.setUTCHours(15);
 					timerNow.setUTCMinutes(00);
 					timerNow.setUTCSeconds(00);
@@ -461,11 +461,11 @@ $(document).ready(function(){
 					progressUpdater(htmlElement,timerNow.getTime());
 					timer(countDate.getTime(),htmlElement,htmlElement1);
 		 	}
-		 	else if(d.getUTCHours() >= 21 && d.getUTCHours() < 22 && d.getUTCMinutes() <=30){
-		 		countDate.setUTCHours(21);
-				countDate.setUTCMinutes(30);
-				countDate.setUTCSeconds(59);
-				timerNow.setUTCHours(21);
+		 	else if(d.getUTCHours() >= 17 && d.getUTCHours() < 19 ){
+		 		countDate.setUTCHours(19);
+				countDate.setUTCMinutes(00);
+				countDate.setUTCSeconds(00);
+				timerNow.setUTCHours(17);
 				timerNow.setUTCMinutes(00);
 				timerNow.setUTCSeconds(00);
 		 		$("#standUp").hide();
